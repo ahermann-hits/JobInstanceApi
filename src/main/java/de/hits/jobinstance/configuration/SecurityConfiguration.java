749dfe4d432e6af621c691ff4c091a33a96fe975
André <andre@chili.fritz.box>
2018-03-09 09:31:06 +0100

    Added Multi-User support to security.

--- src/main/java/de/hits/jobinstance/configuration/SecurityConfiguration.java
+++ src/main/java/de/hits/jobinstance/configuration/SecurityConfiguration.java
@@ -1,11 +1,15 @@
 package de.hits.jobinstance.configuration;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,12 +33,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Value("${api.role.admin}")
 	private String admin;
-	@Value("${api.role.user}")
-	private String user;
+	@Value("#{'${api.role.users}'.split(',')}")
+	private List<String> users;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.httpBasic().and().authorizeRequests()
+				.antMatchers("/v2/api-docs").permitAll()
+				.antMatchers("/swagger-resources/**").permitAll()
+				.antMatchers("/swagger-ui.html").permitAll()
+				.antMatchers("/error/**").permitAll()
 				.antMatchers("/job/api/**").hasRole(ROLE_USER)
 				.antMatchers("/**").hasRole(ROLE_ADMIN)
 				.and().csrf().disable()
@@ -46,19 +54,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		String[] adminProperties = admin.split(":");
 		String adminUser = adminProperties[0];
 		String adminPwd = adminProperties[1];
-
-		String[] userProperties = user.split(":");
-		String userName = userProperties[0];
-		String userPwd = userProperties[1];
-
-		auth.inMemoryAuthentication()
-				.withUser(userName).password(userPwd).roles(ROLE_USER).and()
-				.withUser(adminUser).password(adminPwd).roles(ROLE_USER, ROLE_ADMIN);
+		UserDetailsManagerConfigurer<AuthenticationManagerBuilder, InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder>>.UserDetailsBuilder roles = auth
+				.inMemoryAuthentication().withUser(adminUser).password(adminPwd).roles(ROLE_ADMIN);
 
 		System.out.println("");
-		System.out.println("Using security accounts:");
-		System.out.println("  Admin: " + adminUser + " : " + adminPwd);
-		System.out.println("  User:  " + userName + " : " + userPwd);
+		System.out.println("Using security accounts: ");
+		System.out.println("  Admin:  " + adminUser + " : " + adminPwd);
+		System.out.println("  Users:");
+
+		for (String user : users) {
+			String[] keyValue = user.split(":");
+			String userName = keyValue[0];
+			String userPwd = keyValue[1];
+			roles.and().withUser(userName).password(userPwd).roles(ROLE_USER);
+
+			System.out.println("    User: " + userName + " : " + userPwd);
+		}
+
 		System.out.println("");
 	}
 }
